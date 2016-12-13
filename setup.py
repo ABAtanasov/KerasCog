bb5eae903c99b9826c025d79eb1a59967fed0dc2
AlexAtanasov <alexander.atanasov@yale.edu>
2016-12-13 11:37:04 -0500

    changes/reorganization of the models, bug fixes ongoing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='KerasCog',
+    version='0.1',
+    packages=['', 'models', 'backend'],
+    url='https://github.com/ABAtanasov/KerasCog',
+    license='MIT',
+    author='Alex Atanasov, David Brandfonbrener, Daniel Ehrlich',
+    author_email='alex.atanasov@yale.edu',
+    description='Computational Neuroscience Package using Keras.io'
+)
